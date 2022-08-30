d86b4019805eb80239f6aca2372fc408bdc0a778
Thais Paiva <hello@thaispaiva.info>
2022-08-30 18:09:47 +0200

    params saved on session. pending: search and next step

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,6 @@
 class CitiesController < ApplicationController
   def index
     @cities = City.all
-    raise
     City.search_by_name(params[:query])
   end
 
--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,4 +1,29 @@
 class PreferencesController < ApplicationController
-  def index
+  def cities
+  end
+
+  # saving session params:
+  # method name = the page we are directed to after setting the params below
+  def duration
+    session[:city] = params[:city]
+  end
+
+  def categories
+    session[:duration] = params[:duration]
+  end
+
+  def result
+    session[:categories] = params[:categories]
+    @city = session[:city]
+    @duration = session[:duration]
+    @categories = session[:categories]
+
+    @results = City.find.where()
+
+    # what we need from this is an array with the sight_ids of the sights that
+    # match our search criteria
+
+    # do the filtering thing here
+    # redirect_to first @results.first
   end
 end

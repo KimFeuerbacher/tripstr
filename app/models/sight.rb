5fd0b60b6903567c0207816a3cbaf664364e2872
Kim Feuerbacher <kim.feuerbacher@gmail.com>
2022-08-30 12:54:41 +0200

    last changes to routes

--- app/models/sight.rb
+++ app/models/sight.rb
@@ -1,6 +1,7 @@
 class Sight < ApplicationRecord
   belongs_to :category
   belongs_to :city
+  has_many :stops
   has_many_attached :photos
   validates :name, :latitude, :longitude, :address, :short_description, :long_description, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :cities, only: [:index]
-  resources :preferences, only: [:index]
-  resources :sights, only: [:show] do
-    resources :stops, only: [:create]
+  resources :cities, only: :index
+  resources :preferences, only: :index
+  resources :sights, only: :show do
+    resources :stops, only: :create
   end
-  resources :stops, only: [:destroy]
+  resources :stops, only: :destroy
   resources :itineraries, only: [:show, :index] do
-    resources :stops, only: [:show]
+    resources :stops, only: :show
   end
 end

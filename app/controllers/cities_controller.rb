5e877a23fb39e24c7600f3677de91b7d89c773f9
Anusha Chare <anuchare3@gmail.com>
2022-08-30 16:16:00 +0200

    search

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,8 @@
 class CitiesController < ApplicationController
   def index
+    @cities = City.all
+    raise
+    City.search_by_name(params[:query])
   end
+
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,11 @@
 class City < ApplicationRecord
   has_many :sights
   validates :name, presence: true, uniqueness: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+  against: [ :name],
+  using: {
+    tsearch: { prefix: true }
+  }
 end

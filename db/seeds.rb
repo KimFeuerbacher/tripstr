3286d3b3b1c1fed99dd1eb19ee2fde88d65e4066
Kim Feuerbacher <kim.feuerbacher@gmail.com>
2022-09-07 15:28:30 +0200

    seed improved

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@ require "open-uri"
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts "seeding"
+Stop.destroy_all
 Sight.destroy_all
+Itinerary.destroy_all
 City.destroy_all
 Category.destroy_all
 
